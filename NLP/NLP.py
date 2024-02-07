7253eef5be7712cc300829163309b14306c1a31e
andrewjack7887 <98054446+andrewjack7887@users.noreply.github.com>
2024-02-07 10:14:07 -0500

    sentiment analysis on CNBC and Yahoo Finance headlines using NLTK

--- NLP/NLP.py
+++ NLP/NLP.py
@@ -1,39 +1,146 @@
-# code for NLP
-# code for NLP
-# start with project text file. Pull its tagged words. Also pull its whole text.
-# objective: Learn to open and read in data from files.
-# play with spaCy
-# If necessary at Git Bash or terminal do: pip3 install spacy
+# Source(s):
+# CNBC, Yahoo
+# 
+# Objective:
+# Webscrape the top headlines from each source. Perform a sentiment analysis on them. Aggregate the results and log into json.
 
-import spacy
-# Need line 8 the first time: Then comment it out after the first time you run it:
-nlp = spacy.cli.download("en_core_web_sm")
-nlp = spacy.load('en_core_web_sm')
+# Imports
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-avatarSpeeches = open('example_text.txt', 'r', encoding='utf8')
-words = avatarSpeeches.read()
-wordstrings = str(words)
-print(wordstrings)
+import requests
 
-count=0
-for w in words:
-    count += 1
-    print(count, ": ", w)
+from bs4 import BeautifulSoup
 
-# start playing with spaCy and nlp:
-avatarWords = nlp(wordstrings)
-for token in avatarWords:
-    # if token.pos_ == "VERB":
-    print(token.text, "---->", token.pos_, ":::::", token.lemma_)
+import json
 
-# On windows ctrl / comments out blocks.
-# On mac command / comments out blocks
-myFile = open('example_text.txt', 'r', encoding='utf8')
-doc2 = myFile.read()
-docstring = str(doc2)
-print(doc2)
+# NLTK Downloads
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('vader_lexicon')
 
-nlpMyFile = nlp(docstring)
-for token in nlpMyFile:
-    # print the token and its part of speech tag from spacy
-    print(token.text, "--->", token.pos_)
+## CNBC ##
+
+# print("\n\nLet's look at CNBC. . .\n\n")
+# Get URL
+url = 'https://www.cnbc.com/'
+response = requests.get(url)
+html_content = response.text
+
+# Set up BeutifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find the headline article
+headline_link = soup.find('h2', class_='FeaturedCard-packagedCardTitle').find('a').text
+# print("The top article is " + headline_link + "\nLet's read this article. . . \n\n")
+
+CNBC_article_text = headline_link
+
+# Go to headline article
+# url = headline_link
+# response = requests.get(url)
+# html_content = response.text
+
+# # Set up BeutifulSoup
+# soup = BeautifulSoup(html_content, 'html.parser')
+
+# # Find body element
+# try:
+#     headline_body = soup.find('div', class_='ArticleBody-articleBody').find_all('p')
+# except:
+#     headline_body = soup.find('div', class_='FeaturedContent-articleBody').find_all('p')
+
+# CNBC_article_text = ""
+# for p in headline_body:
+#     CNBC_article_text += p.text
+####
+
+## Yahoo Finance ##
+
+# print("\n\nLet's look at Yahoo Finance. . .\n\n")
+# Get URL
+url = 'https://finance.yahoo.com/'
+response = requests.get(url)
+html_content = response.text
+
+# Set up BeutifulSoup
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# Find the headline article
+headline_link = soup.find(class_="js-content-viewer").find(class_="W(100%)")['alt']
+# print("The top article is " + headline_link + "\nLet's read this article. . . \n\n")
+
+yahoo_article_text = headline_link
+
+# Go to headline article
+# url = headline_link
+# response = requests.get(url)
+# html_content = response.text
+
+# # Set up BeutifulSoup
+# soup = BeautifulSoup(html_content, 'html.parser')
+
+# # Find body element
+# headline_body = soup.find('div', class_='caas-body').find_all('p')
+
+# yahoo_article_text = ""
+# for p in headline_body:
+#     yahoo_article_text += p.text
+####
+
+
+
+
+
+
+# Example financial article
+print("Here are the two headlines: \n")
+headlines = ""
+headlines += CNBC_article_text + "\n"
+headlines += yahoo_article_text
+print(headlines)
+
+# Tokenize the article
+tokens = word_tokenize(headlines)
+
+# Remove stopwords
+stop_words = set(stopwords.words('english'))
+filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+
+# Optional: Lemmatize tokens
+lemmatizer = WordNetLemmatizer()
+lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filtered_tokens]
+
+# print(lemmatized_tokens)
+
+sia = SentimentIntensityAnalyzer()
+sentiment_score = sia.polarity_scores(' '.join(lemmatized_tokens))
+
+positive = sentiment_score['pos'] * 100
+negative = sentiment_score['neg'] * 100
+
+
+# sentiment_quotient = positive / negative
+
+print("\n----Analysis----\n")
+print("Sources: CNBC, Yahoo Finance")
+print(sentiment_score)
+# print("Sentiment Quotient: " + str(sentiment_quotient))
+print("\n------------------------\n")
+
+# Load the existing data
+with open('results.json', 'r') as file:
+    data = json.load(file)
+
+master_key = "data"
+
+data[master_key][headlines] = sentiment_score
+
+with open('results.json', 'w') as file:
+    json.dump(data, file, indent=4)
+
+print("Results logged in results.json")
